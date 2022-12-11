b25e3d0a6ae46b051abbaf0cd737885a17792778
Namesh Dilshan <nameshdilshan@gmail.com>
2022-12-11 10:09:07 +0530

    Initial Commit

---
+++ src/main/java/com/example/demo/controller/BookController.java
@@ -0,0 +1,47 @@
+package com.example.demo.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Book;
+import com.example.demo.repository.BookRepository;
+
+@RestController
+public class BookController {
+
+	@Autowired
+	private BookRepository bookRepository;
+	
+	@PostMapping("/addBook")
+	public String saveBook(@RequestBody Book book) {
+		bookRepository.save(book);
+		return "Added Book with id : "+ book.getId() + "" ;
+	}
+
+	
+	@GetMapping
+	public List<Book> getBooks(){
+		return bookRepository.findAll();
+	}
+	
+	@GetMapping("/{id}")
+	public Optional<Book> getBook(@PathVariable int id){
+		return bookRepository.findById(id);
+	}
+	
+	@DeleteMapping("/{id}")
+	public String deleteBook(@PathVariable int id) {
+		bookRepository.deleteById(id);
+		return "Book deleted with id : " + id;	
+	}
+	
+	
+}
